c7e3f810a97a77fcc0358105beff7a89ec2cc1c6
Vienne Chen <vienne.chen@gmail.com>
2015-04-16 16:02:57 -0400

    adding font-awesome

--- js/app.js
+++ js/app.js
@@ -2,4 +2,7 @@ console.log("app.js loaded");
 
 $(document).ready(function(){
   $('.parallax').parallax();
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal();
 });
+
