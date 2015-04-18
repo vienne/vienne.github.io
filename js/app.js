57c2cc0a9442ca64eb0f62198105bc68f6daa31d
Vienne Chen <vienne.chen@gmail.com>
2015-04-18 12:35:19 -0400

    adding audio player, some css, project pics and mp3

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ console.log("app.js loaded");
 $(document).ready(function(){
   $('.parallax').parallax();
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-  $('.modal-trigger').leanModal();
+  $('.modal-click').leanModal();
+  //music
+  audiojs.events.ready(function() {
+    var as = audiojs.createAll();
+  });
 });
 
